ba52b616a9c2724911da5dc1d5e3b95ff5d2382a
writing.toole <writing.toole@gmail.com>
2019-02-06 20:26:49 -0500

    Update the docs, and elide some commented-out debugging code.

--- lz77/lz77.go
+++ lz77/lz77.go
@@ -7,7 +7,8 @@ import (
 	"log"
 )
 
-// Compress
+// Compress takes a slice of bytes and returns a compressed version of
+// it. Compression is done in 4096 byte blocks for historical reasons.
 func Compress(data []byte) ([]byte, error) {
 	// Is our input less than one block? If so just compress it and be
 	// done. No need for chunking.
@@ -28,7 +29,7 @@ func Compress(data []byte) ([]byte, error) {
 	return ret, nil
 }
 
-// byteLiteral takes a byte and returns the compressed version of it.
+// byteLiteral takes a byte and returns the lz77 encoded version of it.
 func byteLiteral(b byte) []byte {
 	switch {
 	case b == 0:
@@ -48,22 +49,19 @@ func compressBlock(data []byte) []byte {
 	for i := 0; i < len(data); i++ {
 		// Last byte in the input? Encode it and be done.
 		if i == len(data)-1 {
-			//			//log.Printf("writing one byte literal %v", data[i])
 			ret = append(ret, byteLiteral(data[i])...)
 			continue
 		}
 
-		// Have we seen a run already?
+		// Have we seen a run already? If so then encode it.
 		l, offset := findRun(data[i:], data[0:i])
 		if l >= 3 {
-			// 10 bytes is our max.
+			// 10 bytes is our maximum run length.
 			if l > 10 {
 				l = 10
 			}
 			word := uint16(offset<<3+(l-3)) | 0x8000
 			ret = append(ret, byte(word>>8), byte(word&0xff))
-			//			d := data[i-offset : i-offset+l]
-			//log.Printf("%v writing %v byte run %v (%x %x %x off: %v l: %v)", i, l, d, byte(word>>8), byte(word&0xff), word, offset, l)
 
 			i += (l - 1)
 			continue
@@ -72,7 +70,6 @@ func compressBlock(data []byte) []byte {
 		// space + printable? Add in the special byte and be done.
 		if data[i] == ' ' && (data[i+1] >= 0x40 && data[i+1] <= 0x7f) {
 			ret = append(ret, 0x80^data[i+1])
-			//log.Printf("%v writing space pair %v", i, data[i+1])
 			i++
 			continue
 		}
@@ -80,7 +77,6 @@ func compressBlock(data []byte) []byte {
 		// A literal character? Then just pass it on to the output stream.
 		if (data[i] >= 0x09 && data[i] <= 0x7f) || data[i] == 0 {
 			ret = append(ret, data[i])
-			//log.Printf("%v writing character literal %v", i, data[i])
 			continue
 		}
 
@@ -92,7 +88,6 @@ func compressBlock(data []byte) []byte {
 		}
 		ret = append(ret, byte(max))
 		ret = append(ret, data[i:i+max]...)
-		//log.Printf("%v writing %v byte set %v", i, max, data[i:i+max])
 		i += (max - 1)
 		continue
 	}
@@ -100,6 +95,8 @@ func compressBlock(data []byte) []byte {
 	return ret
 }
 
+// findRun looks back in the data we've already compressed to see if
+// we can find a chunk that matches the data that's left to be compressed.
 func findRun(data []byte, seen []byte) (int, int) {
 	// If we don't even have 3 bytes left then we can't have a run.
 	if len(data) < 3 {
@@ -108,7 +105,8 @@ func findRun(data []byte, seen []byte) (int, int) {
 	idx := -1
 	l := -1
 
-	// we can only look back 1024 bytes
+	// we can only look back 1024 bytes, since the offset has to be
+	// encoded in 11 bits.
 	if len(seen) > 1024 {
 		e := len(seen)
 		b := e - 1024
@@ -126,23 +124,21 @@ func findRun(data []byte, seen []byte) (int, int) {
 	return l, idx
 }
 
+// Decompress decompresses a compressed block of data.
 func Decompress(data []byte) ([]byte, error) {
-	// Start off assuming that decompressing a buffer makes the result larger.
+	// Start off assuming that decompressing a buffer makes the result
+	// larger. This is mostly but not always true.
 	ret := make([]byte, 0, len(data)*2)
 	for o := 0; o < len(data); o++ {
-		//		curset := len(ret)
 		b := data[o]
 		switch {
 		case b == 0:
-			//log.Printf("%v reading character literal %v", curset, b)
 			ret = append(ret, b)
 		case (b >= 1 && b <= 8):
 			d := data[o+1 : o+int(b)+1]
 			ret = append(ret, d...)
-			//log.Printf("%v reading %v byte set %v", curset, b, d)
 			o += int(b)
 		case (b >= 0x09 && b <= 0x7f):
-			//log.Printf("%v reading character literal %v", curset, b)
 			ret = append(ret, b)
 		case b >= 0x80 && b <= 0xbf:
 			o++
@@ -154,11 +150,9 @@ func Decompress(data []byte) ([]byte, error) {
 			}
 			d := ret[len(ret)-dist : len(ret)-dist+l]
 			ret = append(ret, d...)
-			//log.Printf("%v reading %v byte run %v", curset, l, d)
 		case b >= 0xc0:
 			ret = append(ret, ' ')
 			ret = append(ret, b^0x80)
-			//log.Printf("%v reading space pair %v", curset, b^0x80)
 		default:
 			log.Fatalf("unknown byte %v", b)
 		}
